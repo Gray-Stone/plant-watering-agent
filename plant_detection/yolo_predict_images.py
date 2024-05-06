3b42b03e47f47b1c7c4f63ccbb6bf7dd810fba48
leo-chen <S8O9R6@u.northwestern.edu>
2024-05-06 01:28:33 -0500

    kinda figured out segmentation stuff

--- plant_detection/yolo_predict_images.py
+++ plant_detection/yolo_predict_images.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # Let user
     parser = argparse.ArgumentParser()
     parser.add_argument("target_img_dir" , type=pathlib.Path ,help="path to Yolo yaml file")
-    parser.add_argument("--model" , default='yolov8n.pt' , type=str ,help="model.pt file")
+    parser.add_argument("--model" , default='yolov8n-seg.pt' , type=str ,help="model.pt file")
 
     args = parser.parse_args()
     target_image_dir : pathlib.Path = args.target_img_dir
@@ -26,17 +26,18 @@ if __name__ == "__main__":
 
     # Load the pretrained model
 
-    model = YOLO(args.model)
+    model = YOLO(args.model , task="segmentation")
     # 58: 'potted plant' this is the existing class.
     # https://docs.ultralytics.com/reference/engine/model/#ultralytics.engine.model.Model.predict
-    results: list[ultralytics.engine.results.Results] = model.predict(target_image_dir,save=True)
+    results: list[ultralytics.engine.results.Results] = model.predict(target_image_dir,save=True , )
 
     # current_path = pathlib.Path().resolve()
 
     for r in results:
         file_path = pathlib.Path(r.path).resolve()
         # maybe save this.
-        r.show()
+        # r.show()
+        print(f"masks \n{r.masks}")
 
 def list_model_classes(model):
     print(f"Model have following classes: {model.names} ")

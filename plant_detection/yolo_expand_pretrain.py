a4dad19a6155baa5a8e9ab294efd9ff4980614a7
leo-chen <S8O9R6@u.northwestern.edu>
2024-06-19 14:54:15 -0500

    pre-train-expand remade

--- plant_detection/yolo_expand_pretrain.py
+++ plant_detection/yolo_expand_pretrain.py
@@ -9,35 +9,48 @@ import ultralytics.engine.results
 
 import pathlib
 
+def list_model_classes(model):
+    print(f"Model have following classes: {model.names} ")
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
+    parser.add_argument("base_model", type = pathlib.Path , help="existing model file")
     parser.add_argument("data_yaml" , type=pathlib.Path ,help="path to Yolo yaml file")
+    parser.add_argument("--proj" , type=str , help="project name" ,default="cont_train")
 
     args = parser.parse_args()
     data_yaml_path : pathlib.Path = args.data_yaml
+    base_model : pathlib.Path = args.base_model
 
+    print(f"using base model {base_model} ")
     print(f"reading data file at {data_yaml_path}")
+
+    if not base_model.is_file():
+        print(f"Given path {base_model} is not a file")
+        exit(1)
+
 
     if not data_yaml_path.is_file():
         print(f"Given path {data_yaml_path} => {data_yaml_path.resolve()} is not a file")
         exit(1)
 
+        
 
-    model = YOLO('renamed_yolov8n.pt')
+    model = YOLO(model=base_model)
     # https://docs.ultralytics.com/reference/engine/model/#ultralytics.engine.model.Model.train
     model.train(
         data=data_yaml_path.resolve(),
-        epochs=200,
-        project="pretrain-expand",
-        pretrained = True,
+        epochs=300,
+        project=args.proj,
+        # WTF is this ? 
+        pretrained = False,
         save=True,
         save_period=5,
         plots = True,
     )
 
-    model.save("pretrained_expand.pt")
+    list_model_classes(model)
+    model.save("cont_training.pt")
 
 
-def list_model_classes(model):
-    print(f"Model have following classes: {model.names} ")

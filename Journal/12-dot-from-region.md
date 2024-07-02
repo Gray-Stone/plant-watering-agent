58258782a8d4c5a0c6362245942759564b57ddea
leo-chen <S8O9R6@u.northwestern.edu>
2024-07-01 22:42:36 -0500

    Better readout of the prediction outcome.

---
+++ Journal/12-dot-from-region.md
@@ -0,0 +1,19 @@
+
+
+## Just use centriod
+
+At the centriod, there usually is the stem of the plant, which could be just fine.
+
+## Find a dot within the region
+
+Find the actual watering dot that land within the region.
+
+
+1. Find center line for abtraty polygon
+
+2. Sample the center line
+
+3. Fit the biggest circle for each sample point that's within outside polygon (watering area mask)
+
+4. pick the point with biggest circle.
+
--- plant_detection/live-predict.py
+++ plant_detection/live-predict.py
@@ -3,6 +3,7 @@
 import argparse
 import pathlib
 
+import dataclasses
 from ultralytics import YOLO
 import ultralytics.engine
 import ultralytics.engine.results
@@ -12,38 +13,6 @@ import pathlib
 import cv2
 import math
 import numpy as np
-
-# https://github.com/ultralytics/ultralytics/issues/561
-def overlay(image, mask, color, alpha, resize=None):
-    """Combines image and its segmentation mask into a single image.
-    https://www.kaggle.com/code/purplejester/showing-samples-with-segmentation-mask-overlay
-
-    Params:
-        image: Training image. np.ndarray,
-        mask: Segmentation mask. np.ndarray,
-        color: Color for segmentation mask rendering.  tuple[int, int, int] = (255, 0, 0)
-        alpha: Segmentation mask's transparency. float = 0.5,
-        resize: If provided, both image and its mask are resized before blending them together.
-        tuple[int, int] = (1024, 1024))
-
-    Returns:
-        image_combined: The combined image. np.ndarray
-
-    """
-    color = color[::-1]
-    colored_mask = np.expand_dims(mask, 0).repeat(3, axis=0)
-    colored_mask = np.moveaxis(colored_mask, 0, -1)
-    masked = np.ma.MaskedArray(image, mask=colored_mask, fill_value=color)
-    image_overlay = masked.filled()
-
-    if resize is not None:
-        image = cv2.resize(image.transpose(1, 2, 0), resize)
-        image_overlay = cv2.resize(image_overlay.transpose(1, 2, 0), resize)
-
-    image_combined = cv2.addWeighted(image, 1 - alpha, image_overlay, alpha, 0)
-
-    return image_combined
-
 
 def overlay_masks(output_frame: np.ndarray, mask_list :list[any] , color =(30,30,255) , alpha = 0.3):
     """Takes an output frame, list of masks. Overlay all masks onto output
@@ -64,6 +33,109 @@ def overlay_masks(output_frame: np.ndarray, mask_list :list[any] , color =(30,30
 
     return output_frame
 
+@dataclasses.dataclass
+class ObjectInfo():
+    pix_xyxy : tuple[int]
+    cls : int
+    confidence : float
+    mask : np.ndarray
+
+class Detector():
+    def __init__(self , model) -> None:
+        self.model = model
+        self.color_list :list[tuple] = [(30,30,255),(30,255,30),(255,30,30)]
+        # Helper used for plotting.
+        self._last_frame_size = None
+        self._blank_color_frames = []
+
+    def DecodeBbox(self,box:ultralytics.engine.results.Boxes):
+        # Because everything in box is
+        # tensor([[446.7046, 365.7438, 651.1225, 635.0509]], device='cuda:0')
+        # Thus need this indexing
+        x1, y1, x2, y2 = box.xyxy[0]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+        obj_class = int(box.cls)
+        xyxy_pix = (x1, y1, x2, y2)
+        confidence = float(box.conf)
+        print(f"box info {xyxy_pix} , cls {obj_class} conf { confidence}")
+
+        return xyxy_pix , obj_class , confidence
+
+    def DecodeMask(self,mask: ultralytics.engine.results.Masks, frame_size:tuple[int,int]):
+        frame_size_y , frame_size_x = frame_size
+        mask_raw = mask.cpu().data.numpy().transpose(1, 2, 0)
+        mask_raw = (mask_raw*255).astype(np.uint8)
+        # print(f" mask raw shape {mask_raw.shape}")
+        mask_scaled = cv2.resize(mask_raw, (frame_size_x, frame_size_y))
+        return mask_scaled
+
+    def DetectOnce(self,frame)->dict[list[ObjectInfo]]:
+        frame_size_y , frame_size_x , _ = frame.shape
+        results = list(self.model.predict(frame , stream=True))
+
+        if len(results) > 1:
+            # I guess the list of result is for handling multiple images.
+            print(f"\n============\nWARING!\n ")
+            print(f"len of result {len(results)}")
+
+        result = results[0]
+        
+        if not result.boxes:
+            return {} , []
+        print(f"len of boxes {len(result.boxes)}")
+        print(f"len of mask {len(result.masks)}")
+
+        obj_info_map = {}
+        obj_info_list = []
+        for box, mask in zip(result.boxes , result.masks):
+            xyxy_pix , obj_class , confidence = self.DecodeBbox(box)
+            segment_mask = self.DecodeMask(mask , (frame_size_y , frame_size_x))
+            obj_info = ObjectInfo(xyxy_pix , obj_class , confidence , segment_mask)
+            if obj_class in obj_info_map:
+                obj_info_map[obj_class].append(obj_info)
+            else:
+                obj_info_map[obj_class] = [obj_info]
+            obj_info_list.append(obj_info)
+
+        return obj_info_map , obj_info_list
+
+    def PlotObjects(self,frame , object_infos:list[ObjectInfo] ):
+
+        BOX_BORDER_THICKNESS = 3
+        MASK_ALPHA = 0.3
+        FONT_SCALE = 0.5
+        FONT_THICKNESS = 2
+        # If size didn't change, then color frame won't change.
+        if self._last_frame_size != frame.shape:
+            self._last_frame_size = frame.shape
+            self._blank_color_frames =[]
+            for color in self.color_list:
+                color_blank = np.zeros(frame.shape , dtype=frame.dtype)
+                color_blank[:] = color
+                self._blank_color_frames.append(color_blank)
+
+        for info in object_infos:
+            # cv2.imshow("debug" , info.mask)
+            x1, y1, x2, y2 = info.pix_xyxy
+            cv2.rectangle(frame , (x1, y1), (x2, y2), self.color_list[info.cls],
+                          BOX_BORDER_THICKNESS)
+            cv2.putText(
+                frame,
+                f"{info.cls}:_{info.confidence:.2f}",
+                (x1, y2),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                FONT_SCALE,
+                self.color_list[info.cls],
+                thickness=FONT_THICKNESS,
+            )
+            color_mask = cv2.bitwise_and(
+                self._blank_color_frames[info.cls],
+                self._blank_color_frames[info.cls],
+                mask=info.mask,
+            )
+            frame = cv2.addWeighted(frame, 1, color_mask, MASK_ALPHA, 0)
+        return frame
+
 
 
 if __name__ == "__main__":
@@ -76,80 +148,46 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     use_image = False
+    cap = None
     if args.image:
         use_image = True
+    else:
+        cap = cv2.VideoCapture(6)
+        if not cap.isOpened():
+            raise IOError("Cannot open webcam")
+
+    cv2.namedWindow("input" , cv2.WINDOW_NORMAL)
+    cv2.namedWindow("labeled_image" , cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("debug" , cv2.WINDOW_NORMAL)
+    # cv2.namedWindow("mask" , cv2.WINDOW_NORMAL)
 
     model = YOLO(args.model)
-    colors = [(30,30,255),(30,255,30),(255,30,30)]
+    detector = Detector(model)
 
-    cap = cv2.VideoCapture(6)
-    cv2.namedWindow("Input" , cv2.WINDOW_NORMAL)
-    cv2.namedWindow("bbox" , cv2.WINDOW_NORMAL)
-    cv2.namedWindow("mask" , cv2.WINDOW_NORMAL)
-
-    if not cap.isOpened():
-        raise IOError("Cannot open webcam")
     while True:
 
         if use_image:
             frame = cv2.imread(args.image)
         else:
             ret, frame = cap.read()
+        
+        obj_info_map , obj_info_list = detector.DetectOnce(frame)
 
-        frame_size_y , frame_size_x , _ = frame.shape
-        results = model.predict(frame , stream=True)
-        i = 0
-        masked_image = frame.copy()
-        bbox_image = frame.copy()
+        for key,val in obj_info_map.items():
+            print(f" Cls: {key} have {len(val)} number of items ")
+        
+        labeled_frame =detector.PlotObjects(frame.copy() , obj_info_list)
 
-        for r in results:
-            # boxes = r.boxes
-            bbox_list = []
-            mask_list = []
-            print(f"Class Name {r.names}")
-            color = colors[i]
-            i = i+1
-            if i == len(colors):
-                i=0
-            # print(f"bboxs \n {r.boxes}")
-            if r.boxes is not None:
-                for box in r.boxes:
-                    print(f"box_xyxy {box.xyxy}")
-                    x1, y1, x2, y2 = box.xyxy[0]
-                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+        cv2.imshow("input" , frame)
+        cv2.imshow('labeled_image', labeled_frame)
 
-                    bbox_list.append((x1, y1, x2, y2))
-
-
-                    # confidence = math.ceil((box.conf[0]*100))/100
-                    # text_origion = [x1, y1]
-                    # cv2.putText(frame, f"{confidence}", text_origion , cv2.FONT_HERSHEY_PLAIN , 2,(255, 0, 255) )
-            # print(f"masks: \n {r.masks}")
-            if r.masks is not None:
-                for mask in r.masks:
-                    mask_raw = mask.cpu().data.numpy().transpose(1, 2, 0)
-                    mask_raw = (mask_raw*255).astype(np.uint8)
-                    # print(f" mask raw shape {mask_raw.shape}")
-                    mask_scaled = cv2.resize(mask_raw, (frame_size_x, frame_size_y))
-                    # print(f" mask scaled shape {mask_scaled.shape}")
-                    mask_list.append(mask_scaled)
-
-            
-            for b in bbox_list:
-                x1, y1, x2, y2 = b
-                cv2.rectangle(bbox_image, (x1, y1), (x2, y2), color, 3)
-
-
-            masked_image = overlay_masks(masked_image ,mask_list ,color)
-
-        cv2.imshow('bbox', bbox_image)
-        cv2.imshow('mask', masked_image)
-
-        c = cv2.waitKey(50)
+        if args.image is None:
+            c = cv2.waitKey(50)
+        else :
+            c = cv2.waitKey()
+            exit(0)
         if c == 27:
             break
 
-
-    # 58: 'potted plant' this is the existing class.
     # https://docs.ultralytics.com/reference/engine/model/#ultralytics.engine.model.Model.predict
     # results: list[ultralytics.engine.results.Results] = model.predict(target_image_dir,save=True)
